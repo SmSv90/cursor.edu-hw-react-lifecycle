323972311066dd857b033f3e3bcfc4c393cf64a5
SmSv90 <smr.stv@gmail.com>
2021-04-19 20:20:33 +0300

    HW #20

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import {Timer} from "./components/Timer"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Timer time={5000} 
+        autostart={false} 
+        step={1000} 
+        onTick={(time) => console.log("Залишилось часу: " + time)}
+        onTimeStart={(timeLeft) => console.log("Таймер запущено!")}
+        onTimePause={(timeLeft) => console.log("Таймер на паузі!")}
+        onTimeEnd={() => console.log("Час вийшов!")} />
+
+      <Timer time={6000000} 
+        autostart={true} 
+        step={2000} 
+        onTick={(time) => console.log("Залишилось часу: " + time)}
+        onTimeStart={(timeLeft) => console.log("Таймер запущено!")}
+        onTimePause={(timeLeft) => console.log("Таймер на паузі!")}
+        onTimeEnd={() => console.log("Час вийшов!")} />
     </div>
   );
 }
